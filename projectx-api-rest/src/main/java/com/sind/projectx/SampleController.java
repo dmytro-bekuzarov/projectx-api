9324253ee27604039fb539dbf0aa32bd1df662d9
Dmytro_Bekuzarov <Dmytro_Bekuzarov@epam.com>
2016-11-01 20:11:30 +0200

    Spring boot added

---
+++ projectx-api-rest/src/main/java/com/sind/projectx/SampleController.java
@@ -0,0 +1,25 @@
+package com.sind.projectx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Dmytro_Bekuzarov
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
